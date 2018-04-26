6ab6a05b6be9f26a7f409bff37de400bcf0609e2
Charles Reed <chreed96@gmail.com>
2018-04-26 16:01:38 -0700

    Add files via upload

---
+++ Assets/Scripts/COlor.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+function Start () {
+	
+		
+}
+
+ function Update () {
+         if (Input.GetMouseButtonDown(0)) {
+             var hit: RaycastHit;
+             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+             
+             if (Physics.Raycast(ray, hit)) {
+                 if (hit.transform.name == "Box1" )gameObject.GetComponent.<Renderer>().material.color = Color.red;
+             }
+         }
+		 }
+
+function OnMouseDown ()
+{
+
+	gameObject.GetComponent.<Renderer>().material.color = Color.red;
+
+}
---
+++ Assets/Size.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var X : int;
+var Y : int;
+
+function Start () {
+X = 100;
+Y = 1;
+
+	
+}
+
+function Update () {
+	
+transform.localScale = new Vector3(X, Y); //We don't changing Z scale now
+
+}
