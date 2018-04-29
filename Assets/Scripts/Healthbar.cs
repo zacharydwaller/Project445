28b39ede33330f18b2b430baa2590031011577d0
Zachary Waller <zacharydwaller@gmail.com>
2018-04-29 15:51:56 -0700

    Cleaned up healthbar and packet code

---
+++ Assets/Scripts/Healthbar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour {
+
+    GameManager gameManager;
+    Slider bar;
+
+	// Use this for initialization
+	void Start () {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        bar = GetComponent<Slider>();
+        bar.maxValue = GameManager.MaxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        bar.value = gameManager.Health;
+	}
+}
---
+++ Assets/Scripts/Packets.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Packets : MonoBehaviour {
+
+    int badID;
+    int[] packetID = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 700 };
+
+    float packetDelay = 1;
+    float nextPacket;
+
+    GameManager gameManager;
+
+    // Use this for initialization
+    void Start () {
+        PacketIDMaker();
+        badID = (Random.Range(0, 10));
+
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        nextPacket = packetDelay;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time >= nextPacket)
+        {
+            nextPacket = Time.time + packetDelay;
+
+            int packet = Mathf.RoundToInt(Random.Range(0, 10));
+            if (packet == badID)
+            {
+                gameManager.Health -= 4;
+            }
+
+            Debug.Log(packetID[packet]);
+            Debug.Log(Mathf.Floor(Time.time));
+        }
+    }
+
+    void PacketIDMaker()
+    {
+        int x;
+        for (int i = 1; i < 10; i++)
+        {
+            x = Mathf.RoundToInt(Random.Range(1, 1000));
+            if (x % 10 != i)
+                while (x % 10 != i)
+                    x = Mathf.RoundToInt(Random.Range(1, 1000));
+
+            packetID[i - 1] = x;
+            //Debug.Log( packetID[i]);
+        }
+
+        packetID[9] = 700;
+
+        var Text1 = GameObject.Find("1Text").GetComponent<TextMesh>();
+        Text1.text = packetID[0].ToString();
+        var Text2 = GameObject.Find("2Text").GetComponent<TextMesh>();
+        Text2.text = packetID[1].ToString();
+        var Text3 = GameObject.Find("3Text").GetComponent<TextMesh>();
+        Text3.text = packetID[2].ToString();
+        var Text4 = GameObject.Find("4Text").GetComponent<TextMesh>();
+        Text4.text = packetID[3].ToString();
+        var Text5 = GameObject.Find("5Text").GetComponent<TextMesh>();
+        Text5.text = packetID[4].ToString();
+        var Text6 = GameObject.Find("6Text").GetComponent<TextMesh>();
+        Text6.text = packetID[5].ToString();
+        var Text7 = GameObject.Find("7Text").GetComponent<TextMesh>();
+        Text7.text = packetID[6].ToString();
+        var Text8 = GameObject.Find("8Text").GetComponent<TextMesh>();
+        Text8.text = packetID[7].ToString();
+        var Text9 = GameObject.Find("9Text").GetComponent<TextMesh>();
+        Text9.text = packetID[8].ToString();
+        var Text10 = GameObject.Find("10Text").GetComponent<TextMesh>();
+        Text10.text = packetID[9].ToString();
+    }
+}
