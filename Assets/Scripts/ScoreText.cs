2dee5db17c379e6d7b96ec4f79d711ffdb4defce
Zachary Waller <zacharydwaller@gmail.com>
2018-05-02 23:26:00 -0700

    Added score text to win screen

---
+++ Assets/Scripts/ScoreText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreText : MonoBehaviour {
+
+    private bool ScoreSet = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!ScoreSet)
+        {
+            var scoreObj = GameObject.Find("ScoreHolder");
+
+            if (scoreObj != null)
+            {
+                var textComponent = GetComponent<Text>();
+                var scoreHolder = scoreObj.GetComponent<ScoreHolder>();
+                textComponent.text = "Your Score: " + scoreHolder.Score.ToString();
+
+                ScoreSet = true;
+            }
+        }
+	}
+}
